fe04ed11b1f9aec4e34110e2e366a95ee05a3710
Adhish Agarwal <adhisha@exzeo.com>
2017-08-19 14:29:14 +0530

    Include service url

--- extension/stock.js
+++ extension/stock.js
@@ -6,8 +6,15 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
   if(message.stockList) {      
       
-      alert(message.stockList[0].company);
-      alert(message.stockList[1].company);
+      //alert(message.stockList[0].company);
+      //alert(message.stockList[1].company);
+
+      $.ajax({
+        url: "http://localhost:8787/stockCount",
+        method: "GET",
+        dataType: "json",
+        success: function(data) { alert(data[0].Stock); }
+    });
   }
   else {
       alert("no data found");
